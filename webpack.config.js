6efadce19ad8d2305635b7b3ca35817bce2541fc
Johnny <name.johnny@mail.ru>
2021-08-05 21:38:45 +0300

    feat: prepare webpack dev middleware

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-const path   = require('path');
-const server = require('./server/server');
-const config = require('./config.json');
-const fs     = require('fs');
+const path   = require('path')
+const WriteFilePlugin = require('write-file-webpack-plugin')
 
 const outPath = path.resolve(__dirname, 'public');
 
 module.exports = {
-    entry : path.resolve(__dirname, 'assets/js/root.js'),
+    entry : {
+        main : [
+            path.resolve(__dirname, 'assets/js/root.js'),
+        ]
+    },
     output : {
         path : outPath,
         filename : 'enex.webpack.js',
@@ -32,48 +34,14 @@ module.exports = {
             }
         ]
     },
-    plugins : [],
-    mode : 'development',
+    performance: {
+        maxEntrypointSize: 4 * 1024 * 1024,
+        maxAssetSize: 4 * 1024 * 1024
+    },
+    plugins : [
+    ],
+    mode : 'production',
     devServer : {
-        contentBase : outPath,
-        port : 1234,
-        watchContentBase : true,
-        hot : true,
-        before: (app) => {
-            app.get('/getLanguage/*', (req, res) => {
-                let urlArr = req.url.split('/');
-                let language = urlArr[urlArr.length - 1];
-                res.json(JSON.parse(fs.readFileSync(`./data/${language}.json`, { encoding : 'utf-8' })));
-            });
-            app.get('/enqlib', (req, res) => {
-                res.send(fs.readFileSync(config.web3_enq_path, { encoding : 'utf-8' }));
-            });
-            app.get(`/api/${config.api_version}/get_dex_pools`, (req, res) => {
-                server.getPools(
-                    result => res.json(result),
-                    error => res.json([])
-                );
-            });
-            app.get(`/api/${config.api_version}/get_tickers_all`, (req, res) => {
-                server.getTokens(
-                    result => res.json(result),
-                    error => res.json([])
-                );
-            });
-            app.get(`/api/${config.api_version}/balance_all`, (req, res) => {
-                server.getBalances( req.query.id,
-                    async result => res.json(await server.convertBalances(result.result)),
-                    error => res.json([])
-                );
-            });
-            app.get('/favicon', (req, res) => {
-                let data = fs.readFileSync(path.resolve('./public/favicon.ico'));
-                res.send(data);
-            });
-            app.get('/swap', (req, res) => {
-                let data = fs.readFileSync(path.resolve('./public/index.html'));
-                res.send(data);
-            });
-        }
+        contentBase : outPath
     }
 };

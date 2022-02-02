6c9037d9f1d9cb7b2a9e490a78b04f4a5f6baa9e
wWrongName <name.johnny@mail.ru>
2022-02-02 20:23:36 +0300

    feat: add buyExact func

--- assets/js/utils/testFormulas.js
+++ assets/js/utils/testFormulas.js
@@ -53,8 +53,12 @@ function getSwapPrice (volume0, volume1, amountIn, pool_fee) {
 }
 
 function revGetSwapPrice (volume0, volume1, amountOut, pool_fee) {
-    let nominator = vp.sub(vp.div(vp.mul(volume0, volume1), vp.sub(volume1, amountOut)), volume0)
-    let denominator = vp.sub(vp.valueToBigInt(1, pool_fee.decimals), pool_fee)
+    let percent = vp.valueToBigInt(1, pool_fee.decimals)
+    let nominator = vp.mul(percent, vp.mul(volume0, amountOut))
+    let denominator = vp.sub(vp.mul(volume1, vp.sub(percent, pool_fee)), amountOut)
+
+    // let nominator = vp.sub(vp.div(vp.mul(volume0, volume1), vp.sub(volume1, amountOut)), volume0)
+    // let denominator = vp.sub(vp.valueToBigInt(1, pool_fee.decimals), pool_fee)
 
     return vp.div(nominator, denominator)
 }

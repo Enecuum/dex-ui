e0664131d93d84f036617d2ada644b1c24d71c72
@djdfy <vtsarev@enecuum.com>
2023-08-10 17:18:04 +0300

    contracts address

--- assets/js/config_prod.js
+++ assets/js/config_prod.js
@@ -31,8 +31,8 @@ export const availableNetworks = [
         type                  : 'eth', //'eth', 'enq'
         testnet               : false, //true false 
         available             : true, //true false 
-        bridgeContractAddress : '0x5A39a0B3B5Eecc2AA986014eaeE2975079463049', //strig, undefined if not used
-        vaultContractAddress  : '0x6195c4d074f18DC65e75BC0c48AB4a163850652a', //strig, undefined if not used
+        bridgeContractAddress : '0xdEF8D9e6FB07b7295657F494f9A01E68dC09922D', //strig, undefined if not used
+        vaultContractAddress  : '0xD2d23E884b19E3C76dA3607B201BebDcE78F16C8', //strig, undefined if not used
         bridgeContractABI     : spaceBridgeABI, //JSON, undefined if not used
         logo                  : undefined, //image, undefined if not used
         explorerURL           : 'https://etherscan.io', // url to explorer mainpage
@@ -46,13 +46,13 @@ export const availableNetworks = [
         type                  : 'eth', //'eth', 'enq'
         testnet               : false, //true false 
         available             : true, //true false 
-        bridgeContractAddress : '0x5A39a0B3B5Eecc2AA986014eaeE2975079463049', //strig, undefined if not used
-        vaultContractAddress  : '0x6195c4d074f18DC65e75BC0c48AB4a163850652a', //strig, undefined if not used
+        bridgeContractAddress : '0xc121545Df72941dDf11a5F23D19B54a6c09a85DF', //strig, undefined if not used
+        vaultContractAddress  : '0xA069dd1aaaA1B55241c7437bbB023A0020897d5B', //strig, undefined if not used
         bridgeContractABI     : spaceBridgeABI, //JSON, undefined if not used
         logo                  : undefined, //image, undefined if not used
         explorerURL           : 'https://polygonscan.com', // url to explorer mainpage
         txPageUrl             : 'https://polygonscan.com/tx/'  // url to tx page without tx hash
-    },/*
+    },
     {
         id                    : 10, // Number, hex ???
         web3ExtensionChainId  : '0xa', //strig as in Metamask, undefined if not used,
@@ -61,8 +61,8 @@ export const availableNetworks = [
         type                  : 'eth', //'eth', 'enq'
         testnet               : false, //true false 
         available             : true, //true false 
-        bridgeContractAddress : '', //strig, undefined if not used
-        vaultContractAddress  : '', //strig, undefined if not used
+        bridgeContractAddress : '0x968B813cE7f35eaba1DCa323ABd12FC29d166FEF', //strig, undefined if not used
+        vaultContractAddress  : '0x02e8A71ad496efca18b2522427096Fb0D849f3Ec', //strig, undefined if not used
         bridgeContractABI     : spaceBridgeABI, //JSON, undefined if not used
         logo                  : undefined, //image, undefined if not used
         explorerURL           : 'https://optimistic.etherscan.io', // url to explorer mainpage
@@ -76,13 +76,13 @@ export const availableNetworks = [
         type                  : 'eth', //'eth', 'enq'
         testnet               : false, //true false 
         available             : true, //true false 
-        bridgeContractAddress : '', //strig, undefined if not used
-        vaultContractAddress  : '', //strig, undefined if not used
+        bridgeContractAddress : '0xdEF8D9e6FB07b7295657F494f9A01E68dC09922D', //strig, undefined if not used
+        vaultContractAddress  : '0xD2d23E884b19E3C76dA3607B201BebDcE78F16C8', //strig, undefined if not used
         bridgeContractABI     : spaceBridgeABI, //JSON, undefined if not used
         logo                  : undefined, //image, undefined if not used
         explorerURL           : 'https://arbiscan.io', // url to explorer mainpage
         txPageUrl             : 'https://arbiscan.io/tx/'  // url to tx page without tx hash
-    },*/
+    },
     {
         id                    : 56, // Number, hex ???
         web3ExtensionChainId  : '0x38', //strig as in Metamask, undefined if not used,
@@ -91,8 +91,8 @@ export const availableNetworks = [
         type                  : 'eth', //'eth', 'enq'
         testnet               : false, //true false 
         available             : true, //true false 
-        bridgeContractAddress : '0x5A39a0B3B5Eecc2AA986014eaeE2975079463049', //strig, undefined if not used
-        vaultContractAddress  : '0x6195c4d074f18DC65e75BC0c48AB4a163850652a', //strig, undefined if not used
+        bridgeContractAddress : '0xdEF8D9e6FB07b7295657F494f9A01E68dC09922D', //strig, undefined if not used
+        vaultContractAddress  : '0xD2d23E884b19E3C76dA3607B201BebDcE78F16C8', //strig, undefined if not used
         bridgeContractABI     : spaceBridgeABI, //JSON, undefined if not used
         logo                  : undefined, //image, undefined if not used
         explorerURL           : 'https://bscscan.com', // url to explorer mainpage
